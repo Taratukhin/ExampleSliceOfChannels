0e9cc1a1742f32bb49427beefebe1751f105bf17
Eugen Taratukhin <taratukhin@gmail.com>
2021-06-30 22:09:39 +0300

    First commit

---
+++ SliceOfChannels.go
@@ -0,0 +1,27 @@
+﻿package main
+import (
+	"fmt"
+)
+
+func ResendToChannels(in chan int, n int) []chan int { // функция возвращает n каналов out и пересылает в них сообщение из канала in, когда оно в нем появляется
+	out := make([]chan int,n)// создадим слайс
+	for i := range out { 	// каждый канал тоже нужно создать
+   		out[i] = make(chan int)
+	}
+	go func () {            // если не запустить горутину, то функция не вернет out, т.к. заблокируется ожиданием сообщения в канале in
+		var msg = <-in  // сообщение во входящий канал ждем, естественно в горутине
+		for i:=0; i<n; i++ {
+			out[i]<-msg
+		}
+	}()
+	return out
+}
+
+func main() {
+	in := make(chan int)
+	out := ResendToChannels(in,5)
+	in <- 111 
+	for i:=0; i<len(out);i++ { 
+		fmt.Printf("Message from channel out[%d]=%d\n",i,<-out[i]) // в этом месте будет ждать сообщения именно в i-й канал поэтому номера каналов будут попорядку
+	}
+}
---
+++ SliceOfChannelsWithWaitGroup.go
@@ -0,0 +1,35 @@
+﻿package main
+import (
+	"fmt"
+	"sync"
+)
+
+func ResendToChannels(in chan int, n int) (out []chan int) { // функция возвращает n каналов out и пересылает в них сообщение из канала in, когда оно в нем появляется
+	out = make([]chan int,n)// создадим слайс
+	for i := range out { 	// каждый канал тоже нужно создать
+   		out[i] = make(chan int)
+	}
+	go func () {            // если не запустить горутину, то функция не вернет out, т.к. заблокируется ожиданием сообщения в канале in
+		var msg = <-in  // сообщение во входящий канал ждем, естественно в горутине
+		for i:=0; i<n; i++ {
+			out[i]<-msg
+		}
+	}()
+	return out
+}
+
+func main() {
+	in := make(chan int)
+	out := ResendToChannels(in,5)
+	var wg sync.WaitGroup // будем ждать окончания запущенных горутин
+	for i:=0; i<len(out);i++ {
+		go func (j int) { 	// приходится передавать номер канала, иначе для всех горутин будет использовано одно значение i 
+					// (последнее значение счетчика после выхода из цикла) 
+			wg.Add(1)	// на одну горутину больше
+			fmt.Printf("Message from channel out[%d]=%d\n",j,<-out[j]) // с какого канала прийдет сообщение первым неизвестно
+			wg.Done() 	// на одну горутину меньше
+		}(i) 
+	}
+	in <- 111
+	wg.Wait()
+}
